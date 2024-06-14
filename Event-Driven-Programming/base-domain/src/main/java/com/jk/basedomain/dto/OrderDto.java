66313a8e12ac8323e7ebc1c3d14c8fb4cc668b84
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-06-14 14:04:12 +0500

    Update OrderDto.java
    Remove getter and setter.

--- Event-Driven-Programming/base-domain/src/main/java/com/jk/basedomain/dto/OrderDto.java
+++ Event-Driven-Programming/base-domain/src/main/java/com/jk/basedomain/dto/OrderDto.java
@@ -9,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,73 +19,4 @@ public class OrderDto {
     private String name;
     private int qty;
     private double price;
-
-   /* public OrderDto() {
-
-    }
-
-    public OrderDto(String orderId, String name, int qty, double price) {
-        this.orderId = orderId;
-        this.name = name;
-        this.qty = qty;
-        this.price = price;
-    }
-
-
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public void setQty(int qty) {
-        this.qty = qty;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderDto orderDto = (OrderDto) o;
-        return qty == orderDto.qty && Double.compare(price, orderDto.price) == 0 && Objects.equals(orderId, orderDto.orderId) && Objects.equals(name, orderDto.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(orderId, name, qty, price);
-    }
-
-    @Override
-    public String toString() {
-        return "OrderDto{" +
-                "orderId='" + orderId + '\'' +
-                ", name='" + name + '\'' +
-                ", qty=" + qty +
-                ", price=" + price +
-                '}';
-    }
-
-    public String getOrderId() {
-        return orderId;
-    }
-*/
 }
